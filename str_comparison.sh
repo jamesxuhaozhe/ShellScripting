ef5efc9a7125beca9d14b1524a8598e59b2c99db
James <jamesxuhaozhe@gmail.com>
2020-04-13 22:44:39 +0800

    Add string comparison

---
+++ str_comparison.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+#
+
+var1="hello"
+var2="world"
+
+if [ $var1 = $var2 ];then
+	echo "Strings are equal"
+else
+	echo "Strings are not equal"
+fi
+
+if [ $var1 != $var2 ];then
+	echo "Strings are not equal!"
+else 
+	echo "Strings are equal"
+fi
+
