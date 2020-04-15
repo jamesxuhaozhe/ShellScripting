ca71c9db2f9058d4cffb7ddd0cd5ce32d38d6b50
Xu Haozhe <xuhaozhe@ibike668.com>
2020-04-15 20:49:13 +0800

    Add another demo of for loop

---
+++ for_command.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+#
+
+FILE=$(ls ~/)
+
+for i in $FILE
+do
+  if [ -d ~/$i ];then
+    echo "~/$i is a directory"
+  elif [ -f ~/$i ]; then
+    echo "~/$i is a file"
+  else
+    echo "Error"
+  fi
+done
