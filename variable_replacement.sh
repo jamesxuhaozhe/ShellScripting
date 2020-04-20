6835dc733513ef3a19bf6090d7da09fa3daf3b07
James <jamesxuhaozhe@gmail.com>
2020-04-20 14:56:11 +0800

    add more replacement demo

--- variable_replacement.sh
+++ variable_replacement.sh
@@ -18,3 +18,16 @@ echo $var3
 var4=${variable_1%ov*}
 
 echo $var4
+
+
+##replace the first string with new string
+
+var5=${variable_1/v/V}
+
+echo $var5
+
+##replace all occurances
+
+var6=${variable_1//v/V}
+
+echo $var6 
