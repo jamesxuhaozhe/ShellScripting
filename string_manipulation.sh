60dd32884597d5b1c2350f39c797f957182973bb
James <jamesxuhaozhe@gmail.com>
2020-04-20 15:19:12 +0800

    add expr index demo

--- string_manipulation.sh
+++ string_manipulation.sh
@@ -12,3 +12,21 @@ echo $len
 len=`expr length "$var1"`
 
 echo $len
+
+
+
+#get the first starting index of the given string within a string
+
+var1="quickstart is an app"
+
+ind=`expr index "${var1}" start`
+
+echo $ind
+
+ind=`expr index "${var1}" arts`
+
+echo $ind
+
+ind=`expr index "${var1}" artz`
+
+echo $ind
