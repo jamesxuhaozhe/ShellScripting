0434a3b0936b42043f14479bd85e43a51aa90de0
James <jamesxuhaozhe@gmail.com>
2020-04-20 15:39:55 +0800

    Add more string manipulation example

--- string_manipulation.sh
+++ string_manipulation.sh
@@ -30,3 +30,33 @@ echo $ind
 ind=`expr index "${var1}" artz`
 
 echo $ind
+
+
+#get the sub string from the specified position
+
+var1="kafka hadoop yan mapreduce"
+
+sub1=${var1:10}
+
+echo $sub1
+
+sub2=${var1:10:5}
+
+echo $sub2
+
+sub3=${var1: -5}
+
+echo $sub3
+
+sub4=${var1: -5:2}
+echo $sub4
+
+
+sub5=`expr substr "${var1}" 10 5`
+echo $sub5
+
+
+
+#when using expr index start from 1. when using ${ss:10}, index start from 0
+
+
