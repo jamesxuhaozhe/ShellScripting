6ebdd76858b3ca7031266b390e253aaddbc8293f
Xu Haozhe <jamesxuhaozhe@gmail.com>
2020-08-22 16:42:02 +0800

    Add 2-2 script

---
+++ learnagain/2_2.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+echo "The first param is $1"
+echo "The second param is $2"
+echo "The third param is $3"
+echo "The fourth param is $4"
+echo "The fifth param is $5"
+
+echo $*
+echo $@
+echo $#
+
