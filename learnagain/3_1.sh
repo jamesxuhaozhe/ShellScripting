84efb48b33f1a6f6577f98ec19f5412e22e3299b
Xu Haozhe <jamesxuhaozhe@gmail.com>
2020-08-22 17:21:58 +0800

    Add 3-1 script

---
+++ learnagain/3_1.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+
+# if command
+# then
+#       do something
+# fi
+
+# the above syntax is a way of testing a command if the command can be executed successfully, then will do something
+if pwd
+then
+    echo "it works"
+fi
