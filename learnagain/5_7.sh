9d6ff213abea04eae3cf87c215af1d59377b1cdf
Xu Haozhe <jamesxuhaozhe@gmail.com>
2020-08-24 22:41:32 +0800

    Add 5-7

--- learnagain/5_7.sh
+++ learnagain/5_7.sh
@@ -9,3 +9,11 @@ echo "This year has passed $(date +%j) days"
 echo "This year has passed $(($(date +%j)/7)) weeks"
 
 
+# check if nginx process is on or not, if not, then start one, the following exmaple is
+# used quite much in the real production environment
+
+nginx_process_num=$(ps -ef | grep nginx | grep -v grep | wc -l)
+if [ $nginx_process_num -eq 0 ]; then
+    #systemctl start nginx
+    echo "start nginx process"
+fi
