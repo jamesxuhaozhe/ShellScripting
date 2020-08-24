edc001aca84e7f3c4458eeb2150a67cad4c72d1b
Xu Haozhe <jamesxuhaozhe@gmail.com>
2020-08-24 22:59:10 +0800

    add variable typing

---
+++ learnagain/5_8.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+# 变量类型定义（用的不是非常多）
+
+# declare a variable to be read only
+
+declare -r var="Hello World"
+
+# declare a variable to be an integer
+
+num1=10
+num2=$num1+90
+
+echo $num2
+
+declare -i num3
+
+num3=$num1+90
+
+echo $num3
+
+# declare an array
+declare -a array
+array=("jone" "mike" "kobe" "James")
+
+# print array content
+# shellcheck disable=SC2068
+echo ${array[@]}
+
+# print the first element of the array
+echo ${array[0]}
+
+# declare a environment variable
+
+declare -x num1
