6be4645a18531b66cf2a1c03b2112adc55ce5e10
Xu Haozhe <jamesxuhaozhe@gmail.com>
2020-08-25 22:42:02 +0800

    library

---
+++ learnagain/6_5.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# 引入我们的库函数
+# 一般来说库函数都会放在lib的文件夹中，然后后缀是.lib
+
+. /Users/haozhexu/workspace/ShellScripting/learnagain/lib/6_5.lib
+
+add 11 23
+reduce 32 10
+divide 30 3
+multiply 40 20
+
+
+sys_info
