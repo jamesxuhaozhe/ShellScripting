bbc64292084fa84780ea6fd203052d8c80c677c9
Xu Haozhe <jamesxuhaozhe@gmail.com>
2020-08-25 11:13:38 +0800

    Add bc demo

--- learnagain/5_10.sh
+++ learnagain/5_10.sh
@@ -3,6 +3,7 @@
 while true
 do
     read -p "Pls input a positive integer: " num
+    #因为 expr 只能用来作为整数的运算，所以给num + 1 这个操作如果可以成功，那么之后的$?一定时返回的0
     expr $num + 1 &> /dev/null
     if [ $? -eq 0 ];then
         if [ `expr $num \> 0` -eq 1 ]; then
